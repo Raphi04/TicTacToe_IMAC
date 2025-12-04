4a82889af6b0734cabe9852d60edfeec45a8e7e4
Raphi04 <raphaelcadete04@gmail.com>
2025-12-04 14:21:19 +0100

    first commit

---
+++ build/CMakeFiles/TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "C:/wamp64/www/IMAC/IMAC1/Prog_Algo/TicTacToe_IMAC/bin/Debug/TicTacToe.exe"
+  "C:/wamp64/www/IMAC/IMAC1/Prog_Algo/TicTacToe_IMAC/bin/Debug/TicTacToe.exe.manifest"
+  "C:/wamp64/www/IMAC/IMAC1/Prog_Algo/TicTacToe_IMAC/bin/Debug/TicTacToe.pdb"
+  "CMakeFiles/TicTacToe.dir/src/test.cpp.obj"
+  "CMakeFiles/TicTacToe.dir/src/test.cpp.obj.d"
+  "libTicTacToe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
